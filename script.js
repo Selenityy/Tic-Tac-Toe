1a286cd3e34f229478a57dc3df527e1ff0fc7e94
Selena <selena.k.rodriguez@gmail.com>
2023-02-27 18:25:12 -0600

    outlined skeleton of each function needed within the game

--- script.js
+++ script.js
@@ -1,51 +1,75 @@
-// Start button toggle function
-// eslint-disable-next-line no-unused-vars
-const toggle = () => {
-    const gameBtn = document.querySelector("#start_btn");
-    const btnText = gameBtn.innerHTML;
-    if(btnText === "Start") {
-        console.log("start"); // later make this start the game
-    } else {
-        console.log("restart"); // later make this wipe everything
+window.onload = function () {
+  const form = document.getElementById("myForm");
+  const startBtn = document.getElementById("startBtn");
+  const gameBtn = document.querySelector("#start_btn");
+  const btnText = gameBtn.innerHTML;
+  const squares = document.querySelectorAll(".grid_child");
+  const players = [];
+
+  // Player creation object
+  const playerFactory = (name, side) => {
+    const sayTurn = () => {
+      document.getElementById("text_display").innerHTML =
+        name + "'s" + " " + "turn.";
     };
-    gameBtn.innerHTML = btnText === "Start" ? "Restart" : "Start";
-};
-
-// add when the start button is clicked, these are grabbed from the form
-const formName = document.getElementById("playerName1").value;
-const formSide = document.querySelector('input[name="sides"]:checked').value;
-const formNameTwo = document.getElementById("playerName2").value;
-const formSideTwo = document.querySelector('input[name="sides"]:not(:checked)').value;
-
-const playerFactory = (name, side) => {
-    const sayName = () => console.log(name); // later have this update the player text sentence
     return {
-        name,
-        side,
-        sayName,
+      name,
+      side,
+      sayTurn,
     };
+  };
+
+  // Start button toggle function
+  // eslint-disable-next-line no-unused-vars
+  const toggle = () => {
+    gameBtn.innerHTML = btnText === "Start" ? "Restart" : "Start";
+  };
+
+  const sayWinner = (winningPlayer) => {
+    let winningPlayer = // add who wins here
+      console.log("Congratulations" + winningPlayer + "!");
+  };
+
+  // Rules to winning
+  const gameOver = () => {};
+
+  // How the game runs
+  const game = () => {
+    const playerSide = player1.side;
+    const mark = (e) => {
+      e.target.innerHTML = playerSide;
+    };
+    squares.forEach((square) => square.addEventListener("click", mark));
+  };
+
+  // Clicking restart button triggers the following:
+  function reset() {
+    form.reset();
+    gameBtn.innerHTML = btnText === "Restart" ? "Start" : "Restart";
+    squares.innerHTML = "";
+  }
+
+  // Clicking start button triggers the following:
+  startBtn.onclick = function (event) {
+    const formName1 = document.getElementById("playerName1").value;
+    const formSide1 = document.querySelector(
+      'input[name="sides"]:checked'
+    ).value;
+    const formName2 = document.getElementById("playerName2").value;
+    const formSide2 = document.querySelector(
+      'input[name="sides"]:not(:checked)'
+    ).value;
+
+    const player1 = playerFactory(formName1, formSide1);
+    const player2 = playerFactory(formName2, formSide2);
+
+    players.push(player1);
+    players.push(player2);
+
+    toggle();
+
+    game();
+
+    event.preventDefault();
+  };
 };
-
-const player1 = playerFactory(formName, formSide);
-const player2 = playerFactory(formNameTwo, formSideTwo);
-
-// let winningPlayer = add who wins here
-
-// eslint-disable-next-line no-unused-vars
-// const sayWinner = (winningPlayer) => {
-//     console.log("Congratulations" + winningPlayer + "!");
-// };
-
-// //add the rules to a game ending here
-// const gameOver = () => {
-
-// };
-
-
-const playerSide = player1.side // or player2.side?
-const mark = (e) => {
-    e.target.innerHTML = playerSide
-};
-
-const squares = document.querySelectorAll(".grid_child");
-squares.forEach(square => square.addEventListener("click", mark));
