1bbe9d42f10c70737629225cd7a1971fe1d59a87
Selena <selena.k.rodriguez@gmail.com>
2023-03-02 17:44:58 -0600

    Fixed the current player object to be called in the other functions

--- script.js
+++ script.js
@@ -1,56 +1,75 @@
 window.onload = function () {
   const form = document.getElementById("myForm");
   const startBtn = document.getElementById("startBtn");
-  const gameBtn = document.querySelector("#start_btn");
-  const btnText = gameBtn.innerHTML;
+  //   const gameBtn = document.querySelector("#startBtn");
+  const btnText = startBtn.innerHTML;
   const squares = document.querySelectorAll(".grid_child");
-  const players = [];
+//   const players = [];
+  let currentPlayer;
 
   // Player creation object
   const playerFactory = (name, side) => {
-    const sayTurn = () => {
-      document.getElementById("text_display").innerHTML =
-        name + "'s" + " " + "turn.";
-    };
     return {
       name,
       side,
-      sayTurn,
     };
   };
 
   // Start button toggle function
   // eslint-disable-next-line no-unused-vars
-  const toggle = () => {
-    gameBtn.innerHTML = btnText === "Start" ? "Restart" : "Start";
-  };
-
-  const sayWinner = (winningPlayer) => {
-    let winningPlayer = // add who wins here
-      console.log("Congratulations" + winningPlayer + "!");
-  };
+  //   const toggle = () => {
+  //     gameBtn.innerHTML = btnText === "Start" ? "Restart" : "Start";
+  //   };
 
   // Rules to winning
   const gameOver = () => {};
 
-  // How the game runs
-  const game = () => {
-    const playerSide = player1.side;
-    const mark = (e) => {
-      e.target.innerHTML = playerSide;
-    };
-    squares.forEach((square) => square.addEventListener("click", mark));
+  // Marks the game board
+  const markSquare = (e) => {
+    console.log(currentPlayer);
+    console.log(currentPlayer.side);
+    if (e.target.getAttribute("mark") === "") {
+      e.target.setAttribute("mark", currentPlayer.side);
+      e.target.innerHTML = currentPlayer.side;
+    }
+  };
+
+  // Begins the listener event, displays whose turn it is, and resets the square attributes
+  const newGame = (player1, player2) => {
+    if (player1.side === "x") {
+      currentPlayer = player1;
+      document.getElementById("text_display").innerHTML =
+        "It's" + " " + currentPlayer.name + "'s" + " " + "turn.";
+    } else {
+      currentPlayer = player2;
+      document.getElementById("text_display").innerHTML =
+        "It's" + " " + currentPlayer.name + "'s" + " " + "turn.";
+    }
+
+    squares.forEach((square) => {
+      square.setAttribute("mark", "");
+      square.addEventListener("click", markSquare);
+    });
+
+    // let element = document.getElementById("restart");
+    // let replayBtn = document.createElement("button");
+    // replayBtn.innerHTML = "Restart"
+    // replayBtn.onclick = function() {
+
+    // };
+    // element.appendChild(replayBtn);
   };
 
   // Clicking restart button triggers the following:
   function reset() {
     form.reset();
-    gameBtn.innerHTML = btnText === "Restart" ? "Start" : "Restart";
+    startBtn.innerHTML = btnText === "Restart" ? "Start" : "Restart";
     squares.innerHTML = "";
+    squares.setAttribute("mark") === "";
   }
 
   // Clicking start button triggers the following:
-  startBtn.onclick = function (event) {
+  startBtn.onclick = function (e) {
     const formName1 = document.getElementById("playerName1").value;
     const formSide1 = document.querySelector(
       'input[name="sides"]:checked'
@@ -60,16 +79,18 @@ window.onload = function () {
       'input[name="sides"]:not(:checked)'
     ).value;
 
-    const player1 = playerFactory(formName1, formSide1);
-    const player2 = playerFactory(formName2, formSide2);
+    let player1 = playerFactory(formName1, formSide1);
+    let player2 = playerFactory(formName2, formSide2);
+    // player1.toString();
+    // player2.toString();
 
-    players.push(player1);
-    players.push(player2);
+    // players.push(player1);
+    // players.push(player2);
 
-    toggle();
+    // toggle();
 
-    game();
+    e.preventDefault();
 
-    event.preventDefault();
+    newGame(player1, player2);
   };
 };
